9e66f05f48818d2062a49cfb3341488b671899ea
Aaron Griffin <aaron@sevco.io>
2022-05-09 15:40:23 -0700

    Fixed git config command

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ "x$3" != "x" ]; then
     echo $3 > .git_credentials
     git config --global credential.helper "store --file $PWD/.git_credentials"
-    git config --global "url.https://github.com/.insteadOf" "ssh://git@github.com/"
+    git config --global --add "url.https://github.com/.insteadOf" "ssh://git@github.com/"
     git config --global --add "url.https://github.com/.insteadOf" "git@github.com:"
 fi
 
